9a3edea7c01a0418708090b986d104f515d65155
Rafael Goncalves <rsgoncalves@gmx.com>
2023-05-30 16:38:07 -0400

    Parameterize DB assembly to enable use with arbitrary metadata sets
    that either contain ontology mappings or that will be mapped per user inputs

--- src/assemble_database.py
+++ src/assemble_database.py
@@ -1,119 +1,142 @@
-import os
 import sqlite3
 import text2term
-import ieugwaspy
+import bioregistry
 import pandas as pd
+from tqdm import tqdm
 from pathlib import Path
-from text2term import Mapper
-from generate_semql_ontology_tables import get_semsql_tables_for_ontology
-from mapping_report_generator import get_mapping_counts
+from metapub import PubMedFetcher
+from generate_ontology_tables import get_semsql_tables_for_ontology
+from generate_mapping_report import get_mapping_counts
 
-__version__ = "0.5.0"
+__version__ = "1.0.3"
+
+text2term_mapping_source_term_col = "SourceTerm"
+text2term_mapping_source_term_id_col = "SourceTermID"
+text2term_mapping_target_term_iri_col = "MappedTermIRI"
+text2term_mapping_score_col = "MappingScore"
 
 
 # Assemble a SQLite database that contains:
-# 1) The original OpenGWAS metadata table containing all traits and associated OpenGWAS DB record identifiers
-# 2) text2term-generated mappings of traits in the OpenGWAS database to terms in the Experimental Factor Ontology (EFO)
-# 3) SemanticSQL tables of EFO that enable searching over traits by leveraging the EFO class hierarchy
-# 4) Counts of how many traits in the metadata were mapped—either directly or indirectly—to each ontology term
-def assemble_database():
-    _clean_existing_resources()
-    target_ontology_name = "EFO"
+# 1) The original user-specified metadata table
+# 3) SemanticSQL tables of the specified ontology that enable search by leveraging the ontology class hierarchy
+# 4) Mappings of the values in the specified column of the metadata table to terms in the specified ontology
+# 5) Counts of how many data points in the metadata were mapped—either directly or indirectly—to each ontology term
+def assemble_database(metadata_df, dataset_name, ontology_name, resource_col, resource_id_col,
+                      ontology_term_iri_col=text2term_mapping_target_term_iri_col,
+                      ontology_semsql_db_url="", ontology_url="", pmid_col="",
+                      ontology_mappings_df=None, mapping_minimum_score=0.7, mapping_base_iris=()):
+    ontology_name = ontology_name.lower()
 
-    # Get SemanticSQL EFO tables
-    edges_df, entailed_edges_df, labels_df, dbxrefs_df, ontology_version = get_semsql_tables_for_ontology(
-        ontology_url="https://s3.amazonaws.com/bbop-sqlite/efo.db",
-        ontology_name=target_ontology_name,
-        tables_output_folder="../resources/",
-        db_output_folder="../resources/",
-        save_tables=True)
-    print("...working with EFO v" + ontology_version)
+    # Get target ontology URL from the specified ontology name
+    if ontology_url == "":
+        ontology_url = bioregistry.get_owl_download(ontology_name)
 
     # Create SQLite database
-    db_name = "../opengwas_trait_search.db"
+    db_name = "../" + dataset_name + "_search.db"
     Path(db_name).touch()
     db_connection = sqlite3.connect(db_name)
 
-    # Fetch OpenGWAS metadata directly from OpenGWAS
-    metadata_file, metadata_df = fetch_opengwas_metadata()
+    # Add the user-given metadata table to the database
+    import_df_to_db(db_connection, data_frame=metadata_df, table_name=dataset_name + "_metadata")
 
-    # Add OpenGWAS metadata table to the database
-    metadata_tbl_cols = "ncase,group_name,year,author,consortium,sex,pmid,population,unit,sample_size,build,ncontrol," \
-                        "trait,id,category,subcategory,ontology,note,mr,nsnp,priority,sd"
-    import_df_to_db(db_connection, data_frame=metadata_df, table_name="opengwas_metadata", table_columns=metadata_tbl_cols)
+    # Get SemanticSQL ontology tables and add them to the database
+    if ontology_semsql_db_url == "":
+        ontology_semsql_db_url = "https://s3.amazonaws.com/bbop-sqlite/" + ontology_name + ".db"
+    edges_df, entailed_edges_df, labels_df, dbxrefs_df, ontology_version = get_semsql_tables_for_ontology(
+        ontology_url=ontology_semsql_db_url,
+        ontology_name=ontology_name.upper(),
+        tables_output_folder="../resources/",
+        db_output_folder="../resources/",
+        save_tables=True)
+    print(f"...working with SemanticSQL build of {ontology_name.upper()} v{ontology_version}")
+    import_df_to_db(db_connection, data_frame=edges_df, table_name=ontology_name + "_edges")
+    import_df_to_db(db_connection, data_frame=entailed_edges_df, table_name=ontology_name + "_entailed_edges")
+    import_df_to_db(db_connection, data_frame=dbxrefs_df, table_name=ontology_name + "_dbxrefs")
 
-    # Add SemanticSQL tables to the database
-    semsql_tbl_cols = "Subject TEXT,Object TEXT"
-    import_df_to_db(db_connection, data_frame=edges_df, table_name="efo_edges", table_columns=semsql_tbl_cols)
-    import_df_to_db(db_connection, data_frame=entailed_edges_df, table_name="efo_entailed_edges", table_columns=semsql_tbl_cols)
-    import_df_to_db(db_connection, data_frame=dbxrefs_df, table_name="efo_dbxrefs", table_columns=semsql_tbl_cols)
+    # Get details (title, abstract, journal) from PubMed about references in the specified PMID column
+    references_df = get_pubmed_details(metadata_df=metadata_df, dataset_name=dataset_name, pmid_col=pmid_col)
+    import_df_to_db(db_connection, data_frame=references_df, table_name=dataset_name + "_references")
 
-    # Use the same version of EFO as used in the SemanticSQL distribution of EFO
-    efo_url = "https://github.com/EBISPOT/efo/releases/download/v" + ontology_version + "/efo.owl"
-
-    # Map the traits to EFO and add the resulting mappings table to the database
-    mappings = map_traits_to_efo(metadata_file, efo_url)
-    import_df_to_db(db_connection, data_frame=mappings, table_name="opengwas_trait_mappings",
-                    table_columns="SourceTermID TEXT,SourceTerm TEXT,MappedTermLabel TEXT,"
-                                  "MappedTermCURIE TEXT,MappedTermIRI TEXT,MappingScore REAL")
+    # Map the values in the specified metadata table column to the specified ontology
+    if ontology_mappings_df is None:
+        ontology_mappings_df = map_metadata_to_ontologies(metadata_df=metadata_df, dataset_name=dataset_name,
+                                                          ontology_url=ontology_url, min_score=mapping_minimum_score,
+                                                          source_term_col=resource_col,
+                                                          source_term_id_col=resource_id_col,
+                                                          base_iris=mapping_base_iris)
+        resource_col = text2term_mapping_source_term_col
+        resource_id_col = text2term_mapping_source_term_id_col
+        ontology_term_iri_col = text2term_mapping_target_term_iri_col
+        ontology_mappings_df.columns = ontology_mappings_df.columns.str.replace(' ', '')
+        import_df_to_db(db_connection, data_frame=ontology_mappings_df, table_name=dataset_name + "_mappings")
 
     # Get counts of mappings
-    counts_df = get_mapping_counts(mappings_df=mappings, ontology_name=target_ontology_name, ontology_iri=efo_url)
-    counts_df.to_csv("../resources/opengwas_efo_mappings_counts.tsv", sep="\t", index=False)
+    counts_df = get_mapping_counts(mappings_df=ontology_mappings_df, ontology_iri=ontology_url,
+                                   source_term_col=resource_col,
+                                   source_term_id_col=resource_id_col,
+                                   mapped_term_iri_col=ontology_term_iri_col)
+    counts_df.to_csv("../resources/" + ontology_name + "_mappings_counts.tsv", sep="\t", index=False)
 
     # Merge the counts table with the labels table on the "iri" column, and add the merged table to the database
     merged_df = pd.merge(labels_df, counts_df, on="IRI")
-    labels_tbl_cols = semsql_tbl_cols + ",IRI TEXT,Direct INT,Inherited INT"
-    import_df_to_db(db_connection, data_frame=merged_df, table_name="efo_labels", table_columns=labels_tbl_cols)
+    import_df_to_db(db_connection, data_frame=merged_df, table_name=ontology_name + "_labels")
+
+
+dtypes = {'int64': 'INTEGER', 'float64': 'REAL', 'object': 'TEXT', 'datetime64': 'TEXT'}
 
 
 # Import the given data frame to the SQLite database through the specified connection
-def import_df_to_db(connection, data_frame, table_name, table_columns):
-    create_table_query = '''CREATE TABLE IF NOT EXISTS ''' + table_name + ''' (''' + table_columns + ''')'''
+# The CREATE TABLE statement is built using the given data frame's column names and inferred data types
+def import_df_to_db(connection, data_frame, table_name):
+    columns = []
+    for column_name, dtype in zip(data_frame.columns, data_frame.dtypes):
+        sql_type = dtypes.get(str(dtype), 'TEXT')
+        column_name = column_name.replace(":", "_")
+        column_name = column_name.replace(" ", "")
+        columns.append(f"`{column_name}` {sql_type}")
+    create_table_query = f'CREATE TABLE IF NOT EXISTS {table_name} ({", ".join(columns)})'
     connection.cursor().execute(create_table_query)
     data_frame.to_sql(table_name, connection, if_exists="replace", index=False)
 
 
-# Map traits in OpenGWAS metadata to terms in EFO
-def map_traits_to_efo(metadata_file, ontology_url):
-    print("Mapping traits in OpenGWAS metadata to EFO...")
-    mappings = text2term.map_file(input_file=metadata_file, csv_columns=("trait", "id"), separator="\t",
-                                  target_ontology=ontology_url, excl_deprecated=True, save_graphs=False,
-                                  max_mappings=1, min_score=0.6, mapper=Mapper.TFIDF,
-                                  save_mappings=True, output_file="../resources/opengwas_efo_mappings.csv",
-                                  base_iris=("http://www.ebi.ac.uk/efo/", "http://purl.obolibrary.org/obo/MONDO",
-                                             "http://purl.obolibrary.org/obo/HP", "http://www.orpha.net/ORDO",
-                                             "http://purl.obolibrary.org/obo/DOID"))
+# Map values in the specified metadata column to terms in the specified ontology set
+def map_metadata_to_ontologies(metadata_df, dataset_name, ontology_url, min_score, source_term_col,
+                               source_term_id_col, base_iris=()):
+    print(f"Mapping values in metadata column '{source_term_col}' to terms in '{ontology_url}'...")
+    source_terms = metadata_df[source_term_col].tolist()
+    if source_term_id_col != "":
+        source_term_ids = metadata_df[source_term_id_col].tolist()
+    else:
+        source_term_ids = ()
+    mappings = text2term.map_terms(source_terms=source_terms, source_terms_ids=source_term_ids,
+                                   target_ontology=ontology_url, excl_deprecated=True, save_graphs=False,
+                                   max_mappings=1, min_score=min_score, save_mappings=True,
+                                   output_file="../resources/" + dataset_name + "_mappings.csv",
+                                   base_iris=base_iris)
     mappings.columns = mappings.columns.str.replace(" ", "")  # remove spaces from column names
+    mappings[text2term_mapping_score_col] = mappings[text2term_mapping_score_col].astype(float).round(decimals=3)
     return mappings
 
 
-# Fetch the OpenGWAS metadata directly from OpenGWAS using ieugwaspy package
-def fetch_opengwas_metadata(metadata_output_file="../resources/opengwas_metadata.tsv"):
-    print("Fetching OpenGWAS metadata...")
-    metadata_dict = ieugwaspy.gwasinfo()
-    metadata_df = pd.DataFrame.from_dict(metadata_dict, orient="index")
-    metadata_df = metadata_df[metadata_df["id"].str.contains("eqtl-a") == False]  # Remove eqtl records
-    metadata_df.to_csv(metadata_output_file, index=False, sep="\t")
-    return metadata_output_file, metadata_df
-
-
-def _clean_existing_resources():
-    _delete_file("../resources/efo.db")
-    _delete_file("../resources/efo_edges.tsv")
-    _delete_file("../resources/efo_entailed_edges.tsv")
-    _delete_file("../resources/efo_dbxrefs.tsv")
-    _delete_file("../resources/efo_labels.tsv")
-    _delete_file("../resources/opengwas_efo_mappings.csv")
-    _delete_file("../resources/opengwas_efo_mappings_counts.tsv")
-    _delete_file("../resources/opengwas_metadata.tsv")
-    _delete_file("../opengwas_trait_search.db")
-
-
-def _delete_file(file):
-    if os.path.isfile(file):
-        os.remove(file)
-
-
-if __name__ == "__main__":
-    assemble_database()
+# Get publication details from PubMed (title, abstract, journal, etc) for the PMIDS in the specified column
+def get_pubmed_details(metadata_df, dataset_name, pmid_col):
+    print("Fetching publication metadata from PubMed...")
+    pmids = metadata_df[pmid_col].dropna().unique()
+    fetch = PubMedFetcher()
+    articles = []
+    for pmid in tqdm(pmids):
+        if pmid != "0" and pmid != "nan":
+            try:
+                article = fetch.article_by_pmid(pmid)
+                title = article.title
+                journal = article.journal
+                year = article.year
+                abstract = article.abstract
+                url = article.url
+                articles.append((pmid, journal, title, abstract, year, url))
+            except Exception as e:
+                print("Unable to fetch article for PMID: " + str(pmid))
+                print(e)
+    references_df = pd.DataFrame(articles, columns=['PMID', 'Journal', 'Title', 'Abstract', 'Year', 'URL'])
+    references_df.to_csv("../resources/" + dataset_name + "_references.tsv", sep="\t", index=False)
+    return references_df
---
+++ src/build_opengwas_db.py
@@ -0,0 +1,43 @@
+import os
+import ieugwaspy
+import pandas as pd
+from assemble_database import assemble_database
+
+
+def delete_existing_resources():
+    _delete_file("../resources/efo.db")
+    _delete_file("../resources/efo_dbxrefs.tsv")
+    _delete_file("../resources/efo_edges.tsv")
+    _delete_file("../resources/efo_entailed_edges.tsv")
+    _delete_file("../resources/efo_labels.tsv")
+    _delete_file("../resources/efo_mappings_counts.tsv")
+    _delete_file("../resources/opengwas_mappings.csv")
+    _delete_file("../resources/opengwas_metadata.tsv")
+    _delete_file("../resources/opengwas_references.tsv")
+    _delete_file("../opengwas_search.db")
+
+
+def _delete_file(file):
+    if os.path.isfile(file):
+        os.remove(file)
+
+
+if __name__ == "__main__":
+    delete_existing_resources()
+
+    # Fetch the OpenGWAS metadata directly from OpenGWAS using ieugwaspy package
+    metadata_dict = ieugwaspy.gwasinfo()
+    metadata_df = pd.DataFrame.from_dict(metadata_dict, orient="index")
+    metadata_df = metadata_df[metadata_df["id"].str.contains("eqtl-a") == False]  # Remove eqtl records
+    metadata_df["pmid"] = metadata_df["pmid"].astype(str).str.replace(".0", "", regex=False)  # remove '.0' from PMIDs
+    metadata_df.to_csv("../resources/opengwas_metadata.tsv", index=False, sep="\t")
+
+    assemble_database(dataset_name="opengwas",
+                      metadata_df=metadata_df,
+                      ontology_name="EFO",
+                      pmid_col="pmid",
+                      resource_col="trait",
+                      resource_id_col="id",
+                      mapping_base_iris=("http://www.ebi.ac.uk/efo/", "http://purl.obolibrary.org/obo/MONDO",
+                                         "http://purl.obolibrary.org/obo/HP", "http://www.orpha.net/ORDO",
+                                         "http://purl.obolibrary.org/obo/DOID"))
